32dd96ecee9b705ce6aa1b8f8ac8b447ce8e3780
Anthony <anthony.maisak@hotmail.com>
2021-08-10 16:41:14 +0300

    Project uploaded.

---
+++ CodeChallenge/CodeChallenge.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using CodeChallenge.Library;
+using System;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new Customers();
+            var cbn = service.GetCustomer("4815162342");
+            System.Console.WriteLine($"Customer succesfully retrieved by id {cbn.Number}: {Environment.NewLine}Name: {cbn.Name}{Environment.NewLine}Email: {cbn.Email}");
+            var cbe = service.GetCustomer2("lostone@email.com");
+            System.Console.WriteLine($"Customer succesfully retrieved by email {cbe.Email}: {Environment.NewLine}Name: {cbe.Name}{Environment.NewLine}Number: {cbe.Number}");
+        }
+    }
+}
---
+++ CodeChallenge/CodeChallenge.Library/DkCustomerService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CodeChallenge.Library
+{
+    public class Customers
+    {
+        public Customer GetCustomer(string id)
+        {
+            var hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://api.dkplus.is/api/v1");
+            hc.DefaultRequestHeaders.Accept.Clear();
+            hc.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            hc.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "3541031f-baf2-4737-a7e8-c66396e5a5e3");
+
+            var m = new HttpRequestMessage(HttpMethod.Get, "https://api.dkplus.is/api/v1/customer/" + id);
+
+            var r = hc.SendAsync(m).Result;
+            var c = JsonSerializer.Deserialize<Customer>(r.Content.ReadAsStringAsync().Result);
+            return c;
+        }
+
+        public Customer GetCustomer2(string email)
+        {
+            var hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://api.dkplus.is/api/v1");
+            hc.DefaultRequestHeaders.Accept.Clear();
+            hc.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            hc.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "3541031f-baf2-4737-a7e8-c66396e5a5e3");
+
+            var m = new HttpRequestMessage(HttpMethod.Get, "https://api.dkplus.is/api/v1/customer/search" + HttpUtility.UrlEncode(email) + "/10");
+
+            var r = hc.SendAsync(m).Result;
+            var c = JsonSerializer.Deserialize<Customer>(r.Content.ReadAsStringAsync().Result);
+            return c;
+        }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Email { get; set; }
+    }
+}
